c5b3f2b64df2d700ae9866b3fb9af8cf52e809b4
Changmin Kim <ckd29672@naver.com>
2023-08-25 15:15:53 +0900

    feat: poster 페이지 step 2 프로토타입 작성

--- src/components/PosterPage/PosterPageBody.js
+++ src/components/PosterPage/PosterPageBody.js
@@ -1,20 +1,33 @@
 import { styled } from "styled-components";
 import PosterPageNav from "./PosterPageNav";
+import PosterPageUsername from "./PosterPageUsername";
 
-const StyleAboutImage = styled.img`
+const StylePosterImage = styled.img`
     display: flex;
     margin: 0px auto;
     flex-shrink: 0;
     width: 970px;
     height: 545px;
 `
-const aboutImage = "/assets/meta/about-meta.png";
+
+const StylePosterUsernameDiv = styled.div`
+    display: flex;
+
+`
+
+const posterImage = "/assets/meta/about-meta.png";
 
 export default function PosterPageBody(){
     return (
         <div>
             <PosterPageNav/>
-            <StyleAboutImage src={aboutImage}/>
+            {/* poster 페이지: step1 */}
+            {/* <StylePosterImage src={posterImage}/> */}
+
+            {/* poster 페이지: step2 */}
+            <StylePosterUsernameDiv>
+                <PosterPageUsername/>
+            </StylePosterUsernameDiv>
         </div>
     );
 }
